7a9b8a935fde57bb85580615af3d61d6f3c78776
raul-2001 <rlqv2001@gmail.com>
2023-10-07 10:05:44 -0400

    react second lesson components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+
+let list = [
+  {
+  id: 1,
+  title: "road to react",
+  },
+
+  {
+    id: 2,
+    title: "road to django",
+    author: "Raul"
+  },
+  {
+    id:3,
+    title: "road to python",
+    author: "Raul"
+
+  },
+  {
+    id: 4,
+    title: "road to AWS",
+    author: "Raul"
+  }
+]
+
+function List(){
+  return(
+  <ul>
+    {list.map(function(item) {
+      return (
+        <li key={item.id}>
+          <span>{item.title}</span>
+          <span>: {item.author}</span>
+        </li>
+        
+      );
+    })
+    }
+  </ul>
+  );
+}
+
+function Search() {
+  return(
+    <div>
+      <label htmlFor='search'>Search</label>
+      <input id='search' type="text" />
+    </div>
+  );
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <h1>My hacker stories</h1>
+      <Search/>
+      <Search/>
+      <hr/>
+      <List/>
+      <List/>
     </div>
   );
 }
